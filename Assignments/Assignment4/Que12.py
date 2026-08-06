6dfe2f6c6fead2eeb5f3133dd96ab31a02353684
saurabh492002-sa <saurabhaghadate2002@gmail.com>
2026-08-06 23:01:20 +0530

    Add files via upload

--- Assignments/Assignment4/Que12.py
+++ Assignments/Assignment4/Que12.py
@@ -2,3 +2,26 @@
 # (Hint : 153 = 1*1*1 + 5*5*5 + 3*3*3 , 1634 = 1*1*1*1 + 6*6*6*6 + 3*3*3*3 + 4*4*4*4)
 
 
+num = int(input("Enter the number : "))
+
+temp = num
+sum = 0
+count = 0
+
+while(temp > 0):
+    count += 1
+    temp //= 10
+# print(count)
+
+temp = num
+
+while(temp > 0):
+    digit = temp % 10
+    # sum = sum + digit ** count
+    sum += digit ** count
+    temp //= 10
+
+if(num == sum):
+    print(f"{num} is Armstrong number.")
+else:
+    print(f"{num} is not Armstrong number.")
